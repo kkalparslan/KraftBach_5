fd4e00cd32b3ad3fbc9c027731d6c4d5a4659347
alparslan <alparslanztk@gmail.com>
2023-02-17 16:16:05 +0300

    added new codes 17.02.23

---
+++ src/Day_1_BenimCalismalarim/ChatGPT.java
@@ -0,0 +1,40 @@
+package Day_1_BenimCalismalarim;
+
+public class ChatGPT {
+
+    public static void main(String[] args) {
+
+        /**
+        y = 8
+        z = lambda x : x * y
+        print (z(6))
+
+        * The output of the code will be 48. The code defines a lambda function named z that takes one argument x
+        and returns the product of x and a variable y that has the value of 8. When z is called with an argument
+        of 6, it returns the product of 6 and 8, which is 48.
+
+        * biraz daha açıklarmısın
+        Tabii, şu satırları adım adım açıklayabilirim:
+
+        y = 8               # değişkene 8 değeri atanıyor
+        z = lambda x: x * y  # bir lambda fonksiyonu tanımlanıyor, x ile y çarpımını döndürüyor
+        print(z(6))         # z fonksiyonu 6 argümanı ile çağrılıyor, 6 * 8 = 48 çıktısı veriyor
+
+        * İlk satırda y adlı bir değişkene 8 değeri atanıyor. İkinci satırda ise lambda ifadesi kullanarak
+        z adlı bir fonksiyon tanımlanıyor. Bu fonksiyon tek bir parametre (x) alıyor ve bu parametreyi y ile
+        çarparak sonucu döndürüyor.
+
+Son satırda, z fonksiyonu 6 argümanı ile çağrılıyor. x parametresi 6 değeri ile değiştiriliyor ve z fonksiyonu 6 * 8 = 48 sonucunu döndürüyor. Bu değer print fonksiyonu ile ekrana yazdırılıyor.
+         * z nin 6 olduğunu print satırındaki (z(6)) ifadesinden mi anlıyoruz
+         * Hayır, z 6 değil, z bir fonksiyondur. z(6) ifadesi z fonksiyonunu 6 argümanı ile çağırdığımızı gösterir.
+
+         z fonksiyonunun tanımında lambda anahtar kelimesi kullanılmıştır, bu da fonksiyonun bir anonim fonksiyon
+         olduğunu ve z adlı değişkenin bu fonksiyonu işaret ettiğini gösterir. lambda fonksiyonları, fonksiyonlar
+         oluşturmak ve adlarına ihtiyaç duymadan çağırmak için kullanılırlar. Bu nedenle, z adlı değişkenin bir
+         fonksiyonu işaret ettiğini belirtmek için parantez içinde bir argüman kullanarak fonksiyonu çağırdık.
+
+         Bu kodda z(6) ifadesi, x parametresine 6 değerini atayarak z fonksiyonunu çağırdığımızı gösterir.
+         Bu nedenle, z(6) ifadesinin değeri 6 * 8 = 48'dir. Sonuç olarak, print(z(6)) ifadesi 48 değerini ekrana yazdırır.
+         */
+    }
+}
--- src/Day_1_BenimCalismalarim/InterviewSorulari.java
+++ src/Day_1_BenimCalismalarim/InterviewSorulari.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class InterviewSorulari {
     public static void main(String[] args) {
 
+
         //String str= "samsung,'OEM Samsung Washing Machine Pulsator Washplate Cap Shipped With WA48J7700AW, WA48J7700AW/A2, WA48J7700AW/AA',20916,samsung,'OEM Samsung Chrome Washing Machine Washplate Pulsator Cap Shipped With WA52M7750AV, WA52M7750AV/A4, WA52M7750AW, WA52M7750AW/A4',91995,samsung, 'SAMSUNG Washing Machine Spring Hanger, DC61-01257M', 22970,samsung,'Samsung DC97-17022B Assy Detergent',32959,samsung,'Samsung DC66-00470A DAMPER SHOCK',29981,samsung,'DC64-00519D Samsung Washing Machine Door Lock Washer Dryer Dishwashe -MP#GH4498 349Y49HBRG9109150',52000,samsung,'Samsung DC97-16991A Assembly Filter',13000";
         String str="samsung,'OEM Samsung Washing Machine Pulsator Washplate Cap Shipped With WA48J7700AW, WA48J7700AW/A2," +
                 "WA48J7700AW/AA',20916,samsung,'OEM Samsung Chrome Washing Machine Washplate Pulsator Cap Shipped With" +
