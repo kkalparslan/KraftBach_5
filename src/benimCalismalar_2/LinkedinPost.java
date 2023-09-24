ed47c1bfd7eee97d80bd3c6c5767b5501fb77b85
Alparslan <alparslanztk@gmail.com>
2023-09-24 23:41:23 +0300

    24.09.23 added LinkedinPost class.

--- src/benimCalismalar_2/LinkedinPost.java
+++ src/benimCalismalar_2/LinkedinPost.java
@@ -3,12 +3,12 @@ package benimCalismalar_2;
 public class LinkedinPost {
 
     public static void main(String[] args){
-        int a = 1234512345;
+        int a = 1234512345; //alabileceği value değerin üzerinde bir value atanırsa compile etmez.
         System.out.println(a);
 
         int x = 5;
         x = 10;
-        System.out.println(x);
+        System.out.println(x); //x değişkeni son tanımlandığı yerdeki değeri döndürür.
     }
 
 }
