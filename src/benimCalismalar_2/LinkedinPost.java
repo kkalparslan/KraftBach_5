2e3cc06eccf85df0e36f3a66449a03473464424c
Alparslan <alparslanztk@gmail.com>
2023-09-24 23:38:15 +0300

    24.09.23 added LinkedinPost class.

---
+++ src/benimCalismalar_2/LinkedinPost.java
@@ -0,0 +1,14 @@
+package benimCalismalar_2;
+
+public class LinkedinPost {
+
+    public static void main(String[] args){
+        int a = 1234512345;
+        System.out.println(a);
+
+        int x = 5;
+        x = 10;
+        System.out.println(x);
+    }
+
+}
