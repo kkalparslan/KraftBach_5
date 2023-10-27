be1cfd907b8a186809d97efa46b6b54126a091bf
Alparslan <alparslanztk@gmail.com>
2023-10-27 09:22:09 +0300

    24.09.23 added LinkedinPost class.

--- src/benimCalismalar_2/LinkedinPost.java
+++ src/benimCalismalar_2/LinkedinPost.java
@@ -6,12 +6,28 @@ public class LinkedinPost {
 
     public static void main(String[] args){
         int a = 1234512345; //alabileceği value değerin üzerinde bir value atanırsa compile etmez.
-        System.out.println(a);
+       // System.out.println(a);
 
         int x = 5;
         x = 10;
         System.out.println(x); //x değişkeni son tanımlandığı yerdeki değeri döndürür.
 
-        System.out.println("Your age is: " + age);
+       // System.out.println("Your age is: " + age);
+
+            for (int i=0; i<=5; i++){
+               // i+=1;
+                System.out.println("Hello World!");
+        }
+
+        String str = "every";
+        str.trim();
+        str.toUpperCase();
+        str.substring(2,3);
+        System.out.println(str);
+
+        System.out.println("hello my friends".split(" ")[2]);
+
+        int[] numbers = {3, 6, 9, 12};
+        System.out.println(numbers[2]);
     }
 }
