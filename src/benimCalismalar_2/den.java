d72b4b912f9d33c10a6a541622b12653d2490c9a
Alparslan <alparslanztk@gmail.com>
2024-03-20 00:14:53 +0300

    20.03.24 arranged some codes

---
+++ src/benimCalismalar_2/den.java
@@ -0,0 +1,13 @@
+package benimCalismalar_2;
+
+public class den {
+    public static void main(String[] args) {
+
+
+            int i = 20;
+            int j = i + 3;
+            i+= j++;
+            System.out.print(++i);
+
+    }
+}
