e2ca32f4a33d8356796302278e6e220ffc96985c
Alparslan <alparslanztk@gmail.com>
2024-12-12 18:29:47 +0300

    arranged some codes

--- src/Day_19_StringMethods/StringMethods.java
+++ src/Day_19_StringMethods/StringMethods.java
@@ -3,7 +3,7 @@ package Day_19_StringMethods;
 public class StringMethods {
     public static void main(String[] args) {
 
-        String str = "durahmanabdurahmanabdurahmanabdurahmanabdurahmanabdurahmanabdurahmanabdurahmanabdurahmanabddurahmana";
+        String str = "gdfddfdfbinselamaahmetbinselamaahmetbinselamaahmetbinselamaahmetbinselamaahmetbinselamaahmetbinselam";
         int harfSayisi=str.length();
         System.out.println("harf sayısı: "+harfSayisi);
 
