f2be7f6ccad529db4f156046e3e2c83513578db5
Alparslan <alparslanztk@gmail.com>
2024-12-17 22:56:48 +0300

    arranged some codes

--- src/Day_19_StringMethods/StringMethods.java
+++ src/Day_19_StringMethods/StringMethods.java
@@ -3,7 +3,7 @@ package Day_19_StringMethods;
 public class StringMethods {
     public static void main(String[] args) {
 
-        String str = "gdfddfdfbinselamaahmetbinselamaahmetbinselamaahmetbinselamaahmetbinselamaahmetbinselamaahmetbinselam";
+        String str = "watermark textwatermark text watermark text waterm";
         int harfSayisi=str.length();
         System.out.println("harf sayısı: "+harfSayisi);
 
